86a3f155f8393d33a79ebef46d946516583c1d11
rpSebastian <cxxuhang@126.com>
2024-06-30 09:36:28 +0000

    测试推理

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -6,4 +6,6 @@ ROOT_DIR=$SCRIPT_DIR/../recipes/inference/local_inference
 
 # python $ROOT_DIR/inference.py --model_name /data1/xuhang/hf_hub/Meta-Llama-3-8B-hf --prompt_file $ROOT_DIR/samsum_prompt.txt 
 
-python $ROOT_DIR/inference.py --model_name /data1/xuhang/hf_hub/Meta-Llama-3-8B-hf --prompt_file $ROOT_DIR/samsum_prompt.txt --peft_model /home/xuhang/LLM/models/llama-3-8B-peft
+# python $ROOT_DIR/inference.py --model_name /data1/xuhang/hf_hub/Meta-Llama-3-8B-hf --prompt_file $ROOT_DIR/samsum_prompt.txt --peft_model /home/xuhang/LLM/models/llama-3-8B-peft
+
+python $ROOT_DIR/inference.py --model_name /data1/xuhang/hf_hub/Meta-Llama-3-8B-hf --peft_model /home/xuhang/LLM/models/llama-3-8B-peft

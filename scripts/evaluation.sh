dd6b086d18309219143ca334625cb0c4240e09c7
rpSebastian <cxxuhang@126.com>
2024-07-01 06:30:46 +0000

    save

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -6,4 +6,4 @@ ROOT_DIR=$SCRIPT_DIR/../recipes/evaluation
 
 # python $ROOT_DIR/eval.py --model hf --model_args pretrained=/data1/xuhang/hf_hub/Meta-Llama-3-8B-hf --tasks hellaswag --device cuda:1   --batch_size 8
 
-python $ROOT_DIR/eval.py --model hf --model_args pretrained=/data1/xuhang/hf_hub/Meta-Llama-3-8B-hf,peft=/home/xuhang/LLM/models/llama-3-8B-peft --tasks hellaswag --num_fewshot 10  --device cuda:2 --batch_size 8
+python $ROOT_DIR/eval.py --model hf --model_args pretrained=/data1/xuhang/hf_hub/Meta-Llama-3-8B-hf,peft=/home/xuhang/LLM/models/llama-3-8B-peft --tasks hellaswag  --device cuda:2 --batch_size 8
